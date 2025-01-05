4eae7968b63dd241f86aa3de3cfbbef7ad291bee
Krishnakaanth3 <krishnakaanth333@gmail.com>
2025-01-05 23:29:58 +0530

    Initial commit

---
+++ strings.py
@@ -0,0 +1,12 @@
+'''
+apple = input ("Enter a string: ")
+apple = int(apple)
+x= apple - 10
+print (x)
+
+fun fact - 0 (ground floor of the building) is the lowest floor of the building in europe. So, technically they started using this principle for indexing an array from 0.
+'''
+def get_last_letter(fruit):
+    return fruit[-1]
+
+print(get_last_letter("banana"))
