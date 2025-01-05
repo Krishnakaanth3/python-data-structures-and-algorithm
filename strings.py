f3ae68fb9f0aea35e16904f8838e8bb5cbf63b2b
Krishnakaanth3 <krishnakaanth333@gmail.com>
2025-01-06 00:24:55 +0530

    added few loc

--- strings.py
+++ strings.py
@@ -6,7 +6,11 @@ print (x)
 
 fun fact - 0 (ground floor of the building) is the lowest floor of the building in europe. So, technically they started using this principle for indexing an array from 0.
 '''
-def get_last_letter(fruit):
-    return fruit[-1]
+fruit = 'banana'
+letter = fruit[0]
+print(letter)
+#find length of a string using len() function
+print(len(fruit))
+#find last letter of a string
+print(fruit[len(fruit)-1])
 
-print(get_last_letter("banana"))
