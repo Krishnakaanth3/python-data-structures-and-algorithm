1bc743f6a3be0f31245171243f7914e273c08127
Krishnakaanth3 <krishnakaanth333@gmail.com>
2025-01-08 00:09:57 +0530

    refactor strings.py to improve string manipulation examples and add character counting functionality

--- strings.py
+++ strings.py
@@ -4,8 +4,8 @@ apple = int(apple)
 x= apple - 10
 print (x)
 
-fun fact - 0 (ground floor of the building) is the lowest floor of the building in europe. So, technically they started using this principle for indexing an array from 0.
-'''
+#fun fact - 0 (ground floor of the building) is the lowest floor of the building in europe. So, technically they started using this principle for indexing an array from 0.
+
 fruit = 'banana'
 letter = fruit[0]
 print(letter)
@@ -13,4 +13,20 @@ print(letter)
 print(len(fruit))
 #find last letter of a string
 print(fruit[len(fruit)-1])
+'''
+word = 'banana'
+count = 0
+for letter in word:
+    if letter == 'a':
+        count = count + 1
+print(count)
 
+#slicing strings
+s = 'Monty Python'
+print(s[0:4])
+print(s[6:7])   
+print(s[6:20])
+print(s[:2])
+print(s[8:])
+print(s[:])
+print(s[12::-1])
