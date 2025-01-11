d2bdc11e0cba230105b696e49b062d5ef0090349
Krishnakaanth3 <krishnakaanth333@gmail.com>
2025-01-11 23:58:24 +0530

    Add List.py to demonstrate list methods and slicing

---
+++ List.py
@@ -0,0 +1,18 @@
+'''
+cheeses = ['Cheddar', 'Edam', 'Gouda']
+numbers = [17, 123]
+empty = []
+print(cheeses, numbers, empty)
+
+print('Edam' in cheeses)
+
+print('Brie' in cheeses)
+
+t = ['a', 'b', 'c', 'd', 'e', 'f']
+print(t[1:3])
+'''
+
+#list methods
+t = ['a', 'b', 'c']
+t.append('d')
+print(t)
