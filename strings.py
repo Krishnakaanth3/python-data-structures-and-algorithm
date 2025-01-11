d20457c79b9ad0f13970e4d9066b044700acc564
Krishnakaanth3 <krishnakaanth333@gmail.com>
2025-01-11 23:51:23 +0530

    Remove unnecessary comment from strings.py

--- strings.py
+++ strings.py
@@ -4,8 +4,6 @@ apple = int(apple)
 x= apple - 10
 print (x)
 
-#fun fact - 0 (ground floor of the building) is the lowest floor of the building in europe. So, technically they started using this principle for indexing an array from 0.
-
 fruit = 'banana'
 letter = fruit[0]
 print(letter)
