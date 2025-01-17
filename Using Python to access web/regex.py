32cc6932505f2fa56d9836a77968454604113358
Krishnakaanth3 <krishnakaanth333@gmail.com>
2025-01-17 06:36:27 +0530

    regex push

--- Using Python to access web/regex.py
+++ Using Python to access web/regex.py
@@ -1,4 +1,14 @@
 import re
+'''
 s = 'A message from csev@umich.edu to cwen@iupui.edu about meeting @2PM'
 lst = re.findall('\S+@\S+', s)
 print(lst)
+
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    x = re.findall('^From .* ([0-9][0-9]):', line)
+    if len(x) > 0: print(x)
+'''
+x = 'We just received $10.00 for cookies.'
+y = re.findall('\$[0-9.]+',x)
