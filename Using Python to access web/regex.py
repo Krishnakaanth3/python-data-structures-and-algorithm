b551d97343d5c42ae5ff34c3ce63a24bbd0193b4
Krishnakaanth3 <krishnakaanth333@gmail.com>
2025-01-18 23:59:38 +0530

    Enhance regex.py to extract email addresses and X-headers from mbox-short.txt

--- Using Python to access web/regex.py
+++ Using Python to access web/regex.py
@@ -9,6 +9,21 @@ for line in hand:
     line = line.rstrip()
     x = re.findall('^From .* ([0-9][0-9]):', line)
     if len(x) > 0: print(x)
-'''
+
 x = 'We just received $10.00 for cookies.'
 y = re.findall('\$[0-9.]+',x)
+≈
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    x = re.findall('[a-zA-Z0-9]\S*@\S*[a-zA-Z]', line)
+    if len(x) > 0:
+        print(x)
+
+
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    if re.search('^X\S*: [0-9.]+', line):
+        print(line)
+'''
